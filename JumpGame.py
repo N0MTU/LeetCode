1e434ed32872d27903f85ec32155ec94b8e6a0a6
Sam S <130558301+N0MTU@users.noreply.github.com>
2023-10-08 10:37:09 +0100

    Create JumpGame.py

---
+++ JumpGame.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        m = 0
+        n = len(nums)
+        for i in range(n):
+            if i > m:
+                return False
+            m = max(m, i + nums[i])
+            if m >= len(nums) - 1:
+                return True
+
+        return False
